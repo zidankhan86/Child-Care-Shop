e1f9c6405f588f041d93cd94dd3c1fdfcc56f9ae
zidankhan86 <zidankhan718@gmail.com>
2024-02-29 23:36:47 +0600

    fix: Trending product

--- resources/views/frontend/pages/home.blade.php
+++ resources/views/frontend/pages/home.blade.php
@@ -6,11 +6,12 @@
 
  @include('frontend.components.navLayer')
  @include('frontend.components.hero')
+ <livewire:trending-product/>
  @include('frontend.components.bannerOne')
  <livewire:latest-product/>
  @include('frontend.components.bannerTwo')
  @include('frontend.components.product')
  @include('frontend.components.banner')
- <livewire:trending-product/>
+ 
 
 @endsection
