bc5e02baa47fbe2efc459873d5fe029672ca54f0
zidankhan86 <zidankhan718@gmail.com>
2024-02-29 23:17:33 +0600

    fix: total users count

--- resources/views/backend/pages/dashboard.blade.php
+++ resources/views/backend/pages/dashboard.blade.php
@@ -37,11 +37,14 @@
                 </div>
             </div>
         </div>
+        @php
+            $user =  App\Models\User::count();
+        @endphp
         <div class="col-xl-3 col-md-6">
             <div class="card bg-danger text-white mb-4">
-                <div class="card-body">NUMBER OF SHOP</div>
+                <div class="card-body">NUMBER OF USERS</div>
                 <div class="card-footer d-flex align-items-center justify-content-between">
-                    <a class="small text-white stretched-link" href="#">View Details</a>
+                    <a class="small text-white stretched-link" href="#">{{$user}}</a>
                     <div class="small text-white"><i class="fas fa-angle-right"></i></div>
                 </div>
             </div>
